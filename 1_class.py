7912e00f0b47af148d378e7941799651d774a4d2
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2023-10-25 23:31:44 +0530

    Created a simple class with attributes and method

---
+++ 1_class.py
@@ -0,0 +1,18 @@
+class Employee:
+    # __init__ method in python is for initialization
+    # basically a constructor
+    def __init__(self, fname, lname, pay):
+        self.fname = fname
+        self.lname=lname
+        self.pay=pay
+        self.email = fname + '.' + lname + '@company.com'
+    
+    def fullname(self):
+        return '{} {}'.format(self.fname, self.lname)
+
+
+emp1 = Employee('Shubham','Masang',100)
+emp2 = Employee('Lalit','Girase',50)
+
+print(emp1.fullname())
+print(emp2.fullname())
